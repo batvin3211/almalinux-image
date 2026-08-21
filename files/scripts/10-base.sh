2bdce252a6dbf6c9a40e780be3cbe5e7c8e0ec05
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2026-08-20 22:33:17 -0400

    Update 10-base.sh

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -9,6 +9,7 @@ set -xeuo pipefail
 # dnf config-manager --set-enabled crb
 dnf install -y --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
 dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
+dnf install -y 'dnf-command(config-manager)'
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/rhel/10/tailscale.repo
 dnf install -y btop distrobox tailscale
 
