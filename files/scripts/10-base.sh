b017cfe6cb064df8339ca9e0051b05831aea9a56
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2026-08-20 21:19:46 -0400

    Update 10-base.sh

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -7,5 +7,30 @@ set -xeuo pipefail
 # Examples:
 # dnf install -y 'dnf-command(config-manager)'
 # dnf config-manager --set-enabled crb
+dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
+dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
+
+dnf install -y btop distrobox epel-release
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install com.brave.Browser \
+         dev.deedles.Trayscale \
+         com.github.tchx84.Flatseal \
+         com.mattjakeman.ExtensionManager \
+         io.github.celluloid_player.Celluloid \
+         org.gpodder.gpodder \
+         org.freac.freac \
+         com.obsproject.Studio \
+         fr.handbrake.ghb \
+         com.makemkv.MakeMKV \
+         org.ferdium.Ferdium \
+         org.freecad.FreeCAD \
+         com.orcaslicer.OrcaSlicer \
+         com.usebottles.bottles -y
+curl -fsSL https://tailscale.com/install.sh | sh
+mkdir -p /nix && \
+	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix -o /nix/determinate-nix-installer.sh && \
+	chmod a+rx /nix/determinate-nix-installer.sh
+
 
 echo "Hello, Atomic AlmaLinux respin world!."
