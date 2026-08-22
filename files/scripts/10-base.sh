36adfa0ad7731cbffcc0414b0367d5e8bf4bcd3f
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2026-08-21 22:12:25 -0400

    Update 10-base.sh

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -9,27 +9,9 @@ set -xeuo pipefail
 # dnf config-manager --set-enabled crb
 dnf install -y --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
 dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
-dnf config-manager --add-repo https://pkgs.tailscale.com/stable/rhel/10/tailscale.repo
-dnf install -y btop distrobox tailscale gnome-tweaks
+dnf install -y btop distrobox gnome-tweaks
 dnf remove -y firefox cockpit
 
-echo "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install com.brave.Browser \
-         dev.deedles.Trayscale \
-         com.github.tchx84.Flatseal \
-		 it.mijorus.gearlever \
-         com.mattjakeman.ExtensionManager \
-         io.github.celluloid_player.Celluloid \
-         org.gpodder.gpodder \
-         org.freac.freac \
-         com.obsproject.Studio \
-         fr.handbrake.ghb \
-         com.makemkv.MakeMKV \
-         org.ferdium.Ferdium \
-         org.freecad.FreeCAD \
-         com.orcaslicer.OrcaSlicer \
-         com.usebottles.bottles -y
-		 curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm" >> /etc/profile
 mkdir -p /nix
 
 echo "Hello, Atomic AlmaLinux respin world!."
